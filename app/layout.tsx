2d0382aeed5e5de6394eb6525751993cd4fd0a25
MrHarigo <timofei.utkin@gmail.com>
2025-12-05 19:46:14 +0900

    feat: add GA tag for tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Outfit, JetBrains_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const outfit = Outfit({
@@ -26,6 +27,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-CN0L2GFMH0"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-CN0L2GFMH0');
+          `}
+        </Script>
+      </head>
       <body
         className={`${outfit.variable} ${jetbrainsMono.variable} font-[family-name:var(--font-outfit)] antialiased`}
       >
